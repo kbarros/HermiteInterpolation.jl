7293824bd39ca0811012fcdf5fb58add27b47439
Documenter.jl <documenter@juliadocs.github.io>
2024-05-11 00:08:06 +0000

    build based on 3f6a3c6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HermiteInterpolation","category":"page"},{"location":"#HermiteInterpolation","page":"Home","title":"HermiteInterpolation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HermiteInterpolation.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HermiteInterpolation]","category":"page"}]
+}
